d2856b3fcfdc42d43a1bf5c656c5d8b67f9db1b4
Iaroslav Iershov <Iaroslav_Iershov@epam.com>
2016-06-07 12:56:46 +0300

    Added force UTF-8 encoding for all requests.

--- ci-bus/src/main/java/com/github/eliogrin/controllers/rest/SkypeRestController.java
+++ ci-bus/src/main/java/com/github/eliogrin/controllers/rest/SkypeRestController.java
@@ -4,11 +4,8 @@ import com.github.eliogrin.controllers.core.ApiController;
 import com.github.eliogrin.dto.EventDto;
 import com.github.eliogrin.models.EventsModel;
 import com.github.eliogrin.models.core.ModelFactory;
-import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,22 +23,16 @@ public class SkypeRestController extends ApiController {
     @Autowired
     private ModelFactory factory;
 
-    @Autowired
-    Gson gson;
-
-    private Logger log = LoggerFactory.getLogger(SkypeRestController.class);
-
     @RequestMapping(path = "/skype/{botId}", method = RequestMethod.POST, consumes = "application/json")
-    public void postEvent(@PathVariable String botId, HttpEntity<String> httpEntity) {
-        log.info("Event for {}, content {}", botId, httpEntity.getBody());
+    public void postEvent(@PathVariable String botId, HttpEntity<JsonArray> httpEntity) {
         EventsModel model = factory.getModel(EventsModel.class);
         model.data(asDtoList(botId, httpEntity.getBody()));
         model.save();
     }
 
-    private List<EventDto> asDtoList(String botName, String stringData) {
+    private List<EventDto> asDtoList(String botName, JsonArray arrayData) {
         List<EventDto> events = new ArrayList<EventDto>();
-        Iterator<JsonElement> iterator = gson.fromJson(stringData, JsonArray.class).iterator();
+        Iterator<JsonElement> iterator = arrayData.iterator();
         while (iterator.hasNext()) {
             EventDto eventDto = new EventDto();
             eventDto.setBot(botName);
